b7fe889c4c093c9aaef49562b5bc2a590c648775
Hunter Lum <hunterlum@gmail.com>
2021-02-14 21:44:56 -0600

    done for now

--- website/app.py
+++ website/app.py
@@ -10,8 +10,6 @@ rm = RecipeManager(bucket = bucket_info['s3_bucket'],recipes_folder = bucket_inf
 
 @app.route('/')
 def main():
-    #rm.update_manager()
-    print('hello world')
     return render_template('index.html',tags=rm.tags)
 
 @app.route('/update')
@@ -21,13 +19,11 @@ def update_manager():
 
 @app.route('/tag/<tag>')
 def recipes_with_tag(tag):
-    #rm.update_manager()
     recipes = rm.list_recipes_with_tag(tag)
     return render_template('list_recipes.html',tags=rm.tags,tag=tag,recipes=recipes)
 
 @app.route('/recipe/<recipe_id>')
 def recipe(recipe_id):
-    #rm.update_manager()
     recipe = rm.get_recipe_from_id(recipe_id)
     return render_template('recipe.html'
         ,title=recipe['Title']
